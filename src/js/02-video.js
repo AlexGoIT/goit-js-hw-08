ab747ef383ad64c5767f2ea35b54430895a19d9b
AlexGoIT <alexandrpuryaev@gmail.com>
2023-04-08 15:49:59 +0300

    Add Task-02 "video"

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate() {
+  player.getCurrentTime().then(time => {
+    localStorage.setItem('videoplayer-current-time', time);
+  });
+}
