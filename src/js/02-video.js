47a1da7c2b842f5d1675a007ad9b4f87c9c60650
alexgoit <alexandrpuryaev@gmail.com>
2023-04-27 14:25:06 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,19 +3,22 @@ import throttle from 'lodash.throttle';
 
 const player = new Player('vimeo-player');
 
-const STORAGE_KEY = "videoplayer-current-time";
+const STORAGE_KEY = 'videoplayer-current-time';
 const currentTime = localStorage.getItem(STORAGE_KEY);
 
 if (currentTime) {
   player.setCurrentTime(currentTime);
-} else {
-  player.setCurrentTime(0);
 }
+
+// function onTimeUpdate() {
+//   player.getCurrentTime().then(seconds => {
+//     localStorage.setItem(STORAGE_KEY, seconds);
+//   });
+// }
+
+function onTimeUpdate({ seconds }) {
+  localStorage.setItem(STORAGE_KEY, seconds);
+}
+
 
 player.on('timeupdate', throttle(onTimeUpdate, 1000));
-
-function onTimeUpdate() {
-  player.getCurrentTime().then(seconds => {
-    localStorage.setItem(STORAGE_KEY, seconds);
-  });
-}
